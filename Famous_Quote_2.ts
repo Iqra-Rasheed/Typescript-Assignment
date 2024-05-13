98afb06b1e9a2233c5e21565e815f36cca5f93ba
Iqra-Rasheed <167316861+Iqra-Rasheed@users.noreply.github.com>
2024-05-13 11:00:01 -0800

    Add files via upload

---
+++ Famous_Quote_2.ts
@@ -0,0 +1,12 @@
+//Famous Quote 2
+//Albert Eintein
+
+
+let famous_person :string ='Albert Einstein ';
+
+let message :string = '“A person who never made a mistake never tried anything new.”';
+
+console.log(`${famous_person} once said, ${message}`);
+
+
+
