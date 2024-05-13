7a79181162703739c492d5ba79dcc5b8e2254284
Iqra-Rasheed <167316861+Iqra-Rasheed@users.noreply.github.com>
2024-05-13 11:02:00 -0800

    Add files via upload

---
+++ Stripping_Number.js
@@ -0,0 +1,6 @@
+//Stripping Number
+const personName = `   \n\tShahid Afridi\t\n   `;
+console.log(personName);
+const without_whitespace = personName.trim();
+console.log(without_whitespace);
+export {};
---
+++ Stripping_Number.ts
@@ -0,0 +1,11 @@
+
+
+       //Stripping Number
+
+       const personName :string =`   \n\tShahid Afridi\t\n   `;
+
+       console.log(personName);
+
+       const without_whitespace :string= personName.trim();
+
+       console.log(without_whitespace);
