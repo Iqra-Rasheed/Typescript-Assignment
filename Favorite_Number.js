00cc7febfe0b1d335fa9ca0d7d1447e92d26a1ea
Iqra-Rasheed <167316861+Iqra-Rasheed@users.noreply.github.com>
2024-05-13 11:24:37 -0800

    Add files via upload

---
+++ Favorite_Number.js
@@ -0,0 +1,5 @@
+//Favorite Number
+const Favorite_Number = 100;
+const message = 'Here is your favorite number ' + Favorite_Number;
+console.log(message);
+export {};
---
+++ Favorite_Number.ts
@@ -0,0 +1,9 @@
+
+
+   //Favorite Number
+
+   const Favorite_Number :number = 100 ;
+
+   const message :string = 'Here is your favorite number ' + Favorite_Number ;
+
+   console.log(message);    
