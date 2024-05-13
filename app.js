6f386ad3cb1422f6d8192de3a9f1be618d0ad51f
Iqra-Rasheed <167316861+Iqra-Rasheed@users.noreply.github.com>
2024-05-13 10:55:58 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+//lowerv case
+//let personName: string ="Baber"
+//console.log("lower case:",personName.toLowerCase());
+//upper case
+//console.log("uppercase:", personName.toLocaleUpperCase());
+//Title case
+//console.log("titlecase :", personName.replace(/\bw/g,c=> c.toUpperCase()));
+//Step:1 Create Variable
+let personNames = "iqra";
+personNames = personNames.toUpperCase();
+console.log(personNames);
+personNames = personNames.toLowerCase();
+console.log(personNames);
+function toTitleCase(str) {
+    const firstCharInUpperCase = str.charAt(0).toUpperCase();
+    console.log("firstChar:", firstCharInUpperCase);
+    const charsInlowerCase = str.slice(1).toLowerCase();
+}
